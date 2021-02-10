e1f08a81c7673cde92498448f0dcbbc9993fd6b4
Elias Macedo da Silva <elias.macedo@geru.com.br>
2021-02-10 13:23:48 -0300

    Inclusion of tests, improved implementation and improved README

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# test-petstore-api-supertest
+Study project for REST API tests using API https://petstore.swagger.io/ made with [JEST](https://jestjs.io/) and [SuperTest](https://www.npmjs.com/package/supertest).
+
 # Install
 `npm i`
 
--- tests/user.test.js
+++ tests/user.test.js
@@ -24,23 +24,29 @@ describe("POST /user", () => {
 
 describe("GET /user/:username", () => {
     it("Search user 'Bem Bolado' by username and return 200", () => {
-        var username="be.bolado";
+        var username = "be.bolado";
+        var responsePayload = {
+            "id": 9013685127327373747,
+            "username": "be.bolado",
+            "firstName": "Bem",
+            "lastName": "Bolado",
+            "email": "biloxeh350@boldhut.com",
+            "password": "biloxeh350",
+            "phone": "1198765-4321",
+            "userStatus": 0
+        }
         return request(ApiUrl)
             .get("/user/" + username)
             .expect(200)
             .then(response => {
-                expect(response.status).toEqual(200);
-                expect(response.body.firstName).toEqual("Bem");
-                expect(response.body.lastName).toEqual("Bolado");
-                expect(response.body.email).toEqual("biloxeh350@boldhut.com");
+                expect(response.body).toEqual(responsePayload);
             });
-
     });
 });
 
 describe("GET /user/:username", () => {
     it("Search for invalid user and return 404", () => {
-        var username="mr.mxyzptlk";
+        var username = "mr.mxyzptlk";
         var responsePayload = {
             code: 1,
             type: "error",
@@ -57,5 +63,34 @@ describe("GET /user/:username", () => {
     });
 });
 
+describe("PUT /user:username", () => {
+    it("Update email, password and password for user 'ratonhnhake.ton' and return 200", () => {
+        var username = "ratonhnhake.ton";
+        var responsePayload = {
+            code: 200,
+            type: "unknown",
+            message: "9013685127327374000"
+        }
+        //ToDo: Investigate why the Return ID differs from the request ID
 
+        return request(ApiUrl)
+            .put("/user/" + username)
+            .send({
+                "id": 9013685127327374203,
+                "username": "ratonhnhake.ton",
+                "firstName": "Ratonhnhake",
+                "lastName": "Ton",
+                "email": "ratonhnhake.ton@creed.com",
+                "password": "Creed",
+                "phone": "1194567-1234",
+                "userStatus": 0
+            })
+            .expect(200)
+            .then(response => {
+                expect(response.status).toEqual(200);
+                expect(response.body).toEqual(responsePayload)
+            });
+
+    });
+});
 
